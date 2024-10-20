1f52d620abb6dbe729b6fe90e5219a651130c025
Ömer Özgül <omerozgul@omermacbook.local>
2024-10-20 11:25:54 +0300

    first commit

---
+++ src/test/java/com/omerozgul/demo_socketio/DemoSocketioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omerozgul.demo_socketio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSocketioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
